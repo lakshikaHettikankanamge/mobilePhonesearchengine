9757ad5922d4b8c24c838f2f64ea8b62a5a37592
lakshika <lakshikahettikankanamge@gmail.com>
2019-11-15 12:04:09 +0530

    initial commit after removing duplicate folder

--- server/config/keys.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    // mongoURI:'mongodb+srv://lakshikahettikankanamge:iloverivers1@@tmcluster-n02oq.mongodb.net/test?retryWrites=true'
-    mongoURI:'mongodb://localhost:27017/mPhone'
- };
